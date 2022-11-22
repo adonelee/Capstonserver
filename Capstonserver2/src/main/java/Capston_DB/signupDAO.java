7465ec5f8710e629afdb263d9f27edf660ef0474
거부길 <sopens@naver.com>
2022-11-22 10:38:26 +0900

    signup

---
+++ Capstonserver2/src/main/java/Capston_DB/signupDAO.java
@@ -0,0 +1,50 @@
+package Capston_DB;
+
+import java.sql.*;
+
+public class signupDAO {
+		//test code
+		public String SignUP(String id, String pw, String name, String sex, String phone) {
+			String SQL1 = "select * from user where USER_ID = ?";
+			String SQL2 = "insert into user values(?,?,?,?,?)";
+			
+			try {
+			    //DB 연결 
+				Connection conn = Capston_Connection.GetDB();
+				PreparedStatement ptstn = conn.prepareStatement(SQL1);
+				ptstn.setString(1, id);
+				ResultSet rs = ptstn.executeQuery();
+				
+				if(rs.next()) {
+					ptstn.close();
+					return "중복";
+				}
+				else {
+				    try {
+				    	ptstn.close();
+				    	PreparedStatement ptstm = conn.prepareStatement(SQL2);
+						
+						ptstm.setString(1, name);
+						ptstm.setString(2, id);
+						ptstm.setString(3, pw);
+						ptstm.setString(4, sex);
+						ptstm.setString(5, phone);
+					    ptstm.executeUpdate();
+					    
+					    Statement state = conn.createStatement();
+						
+					    state.close();
+					    ptstm.close();
+						
+					    return "성공";
+				    }catch (Exception e){
+						System.out.println(e.getMessage());
+						return "삽입실패";
+				     }		    
+		         }
+			 }catch(Exception e) {
+				System.out.println(e.getMessage());
+				return "검색실패";
+				}
+			}
+}
---
+++ Capstonserver2/src/main/webapp/signup.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="UTF-8" import="Capston_DB.*"%>
+<%
+ request.setCharacterEncoding("UTF-8");
+
+String id = request.getParameter("id");
+String pw = request.getParameter("pw");
+String name = request.getParameter("name");
+String sex = request.getParameter("sex");
+String phone = request.getParameter("phone");
+
+System.out.println(id);
+System.out.println(pw);
+System.out.println(name);
+System.out.println(sex);
+System.out.println(phone);
+
+String result = new signupDAO().SignUP(id,pw,name,sex,phone);
+
+out.print(result);
+%>
