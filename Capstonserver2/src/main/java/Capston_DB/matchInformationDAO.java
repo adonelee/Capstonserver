49f2cb9774c4a36afdab6f8a54b3ac4491c90251
adonelee <akrso520@naver.com>
2022-12-01 02:00:34 +0900

    Update matchInformationDAO.java

--- Capstonserver2/src/main/java/Capston_DB/matchInformationDAO.java
+++ Capstonserver2/src/main/java/Capston_DB/matchInformationDAO.java
@@ -4,8 +4,8 @@ import java.sql.*;
 
 public class matchInformationDAO{
 	public String getMatchInformation(int match_number) {
-		String SQL1 = "select match_number, creater_id, match_title"
-				+ " exercise_type, match_type, match_time, recruit_person"
+		String SQL1 = "select match_number, creater_id, match_title,"
+				+ " exercise_type, match_type, match_time, recruit_person,"
 				+ " match_sex, match_major from match_info where match_number = ?";
 		
 		String matchInformation = "";
@@ -24,7 +24,7 @@ public class matchInformationDAO{
 				
 			//매칭번호 : 생성자, 매치타이틀, 운동 종류, 매칭 종류, 매칭 시간, 모집 인원, 성별, 학과 
 			matchInformation += rs.getInt(1) +":"+ rs.getString(2) +","+ rs.getString(3) +","+ rs.getString(4)
-			+","+ rs.getString(5) +","+ rs.getString(6) +","+ rs.getInt(7) +","+ rs.getString(8) +","+ rs.getString(9);
+			+","+ rs.getString(5) +","+ rs.getString(6) +","+ rs.getInt(7) +","+ rs.getString(8) +","+ rs.getString(9).toString();
 			
 			ptstn.close();
 			    
